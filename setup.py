4dbac83bfefd72da8f5d38d647de3686af5351d9
StroupBSlayen <strouprd@gmail.com>
2019-02-20 16:52:45 -0800

    Update to setup

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Topic :: JSON',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
