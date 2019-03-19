a954a68d5ad4e3ef96cb9f3f04200478e5e305bb
StroupBSlayen <strouprd@gmail.com>
2019-03-19 12:37:48 -0700

    Fixed install for v1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='jthon',
-    version='1.0.0',
+    version='1.0.1',
     description='A JSON helper for Python',
     packages=['jthon'],
     url='https://github.com/stroupbslayen/Jthon',
